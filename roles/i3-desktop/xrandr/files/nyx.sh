cc1304f28373631d597624c5043ebe8385dd4ea5
David Chandler <contact@dechandler.io>
2018-08-08 21:59:19 -0500

    updated desk configuration

--- roles/i3-desktop/xrandr/files/nyx.sh
+++ roles/i3-desktop/xrandr/files/nyx.sh
@@ -9,10 +9,18 @@ function default () {
 }
 
 function docked () {
+    #xrandr \
+    #--output DVI-I-1-1 --mode 1920x1080 --pos 0x0 --rotate normal \
+    #--output DVI-I-2-2 --mode 1920x1080 --pos 1920x0 --rotate normal \
+    #--output eDP-1 --primary --mode 1920x1080 --pos 3840x576 --rotate normal \
+    #--output HDMI-1 --off \
+    #--output DP-2 --off \
+    #--output DP-1 --off
+
     xrandr \
-    --output DVI-I-1-1 --mode 1920x1080 --pos 0x0 --rotate normal \
-    --output DVI-I-2-2 --mode 1920x1080 --pos 1920x0 --rotate normal \
-    --output eDP-1 --primary --mode 1920x1080 --pos 3840x576 --rotate normal \
+    --output DVI-I-1-1 --mode 1920x1080 --pos 0x840 --rotate normal \
+    --output DVI-I-2-2 --mode 1920x1080 --pos 1920x0 --rotate right \
+    --output eDP-1 --primary --mode 1920x1080 --pos 3000x1376 --rotate normal \
     --output HDMI-1 --off \
     --output DP-2 --off \
     --output DP-1 --off
