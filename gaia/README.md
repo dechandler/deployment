e7958001fe375e30a7b3d7990a06f8f60121aaa1
David Chandler <contact@dechandler.net>
2019-02-19 17:18:57 -0600

    gaia subnet segregation, fix jump user, and automatic updates

---
+++ gaia/README.md
@@ -0,0 +1,5 @@
+# Gaia Deployment
+
+Gaia is an x86 custom router running Alpine Linux.
+
+Copy this directory and run ./gaia-init.sh
--- gaia/gaia-init.sh
+++ gaia/gaia-init.sh
@@ -9,7 +9,7 @@ service networking restart
 echo 'net.ipv4.ip_forward = 1' > /etc/sysctl.d/01-router.conf
 sysctl -p /etc/sysctl.conf
 
-# Install iptables and create rules
+# IPTABLES: Install and Create Rules
 apk add iptables
 service iptables stop
 DEST="/etc/iptables/rules-save"
@@ -18,30 +18,24 @@ chmod 600 $DEST
 rc-update add iptables
 service iptables start
 
-# Install and configure dnsmasq
+# DNSMASQ: Install and Configure
 apk add dnsmasq
 cat dnsmasq dnsmasq.static-hosts > /etc/dnsmasq.conf
 rc-update add dnsmasq
 service dnsmasq restart
 
-# Create and configure sshd jump service
-ln -s /etc/init.d/sshd /etc/init.d/sshd.jump
-echo 'SSHD_CONFDIR="/etc/ssh/jump"' > /etc/conf.d/sshd.jump
+# SSHD: Maintenance Service
+cp -f sshd_config.maint /etc/ssh/sshd_config
+echo 'rc_need="net"' >> /etc/conf.d/sshd
+service sshd restart
+
+# SSHD: Jump Service
 mkdir /etc/ssh/jump
 cp -f sshd_config.jump /etc/ssh/jump/sshd_config
+ln -s /etc/init.d/sshd /etc/init.d/sshd.jump
+echo 'cfgfile="/etc/ssh/jump/sshd_config"' > /etc/conf.d/sshd.jump
 rc-update add sshd.jump
 service sshd.jump restart
-
-# Create jump user
-adduser -D -H -h /dev/null -s /bin/true jump
-DEST="/etc/ssh/jump/jump.authorized_keys"
-cp -f authorized_keys.jump $DEST
-chown root:jump $DEST
-chmod 640 $DEST
-
-# Configure sshd for maintenance access
-cp -f sshd_config.maint /etc/ssh/sshd_config
-service sshd restart
 
 # Create maintenance user
 adduser -u 2000 -D maintenance
@@ -49,3 +43,25 @@ DEST="/etc/ssh/authorized_keys"
 cp -f authorized_keys.maint $DEST
 chown root:maintenance $DEST
 chmod 640 $DEST
+
+# Create jump group and user
+addgroup jump
+adduser -D -H -h /dev/null -s /bin/true -G jump jump-david
+passwd -u jump-david
+DEST="/etc/ssh/jump/jump-david.authorized_keys"
+cp -f authorized_keys.jump-david $DEST
+chown root:jump $DEST
+chmod 640 $DEST
+
+# Automatically update and selectively reboot via cron
+echo -e '#!/bin/ash
+apk update && apk upgrade
+' > /etc/periodic/daily/update
+echo -e '#!/bin/ash
+find /boot -name "initramfs-vanilla" -mtime -7 | grep i || reboot
+' > /etc/periodic/weekly/reboot
+chmod 755 /etc/periodic/*/*
+
+apk update
+apk upgrade
+reboot
